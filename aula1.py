9ee70c2943c3ff81f5910de0a3bc76b31cc260fc
reinaldo.silva <reinaldo.silvarrrb@gmail.com>
2026-03-22 20:59:55 -0300

    exibicao soma e media

--- aula1.py
+++ aula1.py
@@ -3,4 +3,5 @@ n2=int(input("Digite segundo numero"))
 n3=int(input("Digite segundo numero"))
 soma=n1+n2+n3
 media=soma/3
-print("a soma e:", media)
+print("a soma e:", soma)
+print("a media e:", media)
